660337c5d0984e4cfe3443eae59dc0f772b20cf3
rahul kirpekar <rahul.kirpekar@gmail.com>
2024-05-31 19:53:58 +0530

    prop db

--- src/jdbctopic/util/DBConnection.java
+++ src/jdbctopic/util/DBConnection.java
@@ -1,27 +1,49 @@
 package jdbctopic.util;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DBConnection 
 {
 	// 1) Make credentials
-	private static final String URLNAME      = "jdbc:mysql://localhost:3306/aprilbatch";
-	private static final String DRIVERCLASS  = "com.mysql.cj.jdbc.Driver";
-	private static final String USERNAME     = "root";
-	private static final String PASSWORD	 = "root";
+//	private static final String URLNAME      = "jdbc:mysql://localhost:3306/aprilbatch";
+//	private static final String DRIVERCLASS  = "com.mysql.cj.jdbc.Driver";
+//	private static final String USERNAME     = "root";
+//	private static final String PASSWORD	 = "root";
 	
 	// 2) create getDBConnection method
 	public static void getDBConnection() 
 	{
+		Properties prop =  new Properties();
+
+		try 
+		{
+			
+			FileInputStream fin = new FileInputStream("C:\\Users\\Royal\\eclipse-workspace\\aprilocjpbatch23\\dbconfig.properties");
+
+			prop.load(fin);
+
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+		
+		String urlName    = prop.getProperty("URLNAME").trim();
+		String driverClass= prop.getProperty("DRIVERCLASS").trim();
+		String userName    = prop.getProperty("USERNAME").trim();
+		String password    = prop.getProperty("PASSWORD").trim();
+		
 		Connection conn = null;
 		try 
 		{
 			// 3) Load Driver class
-			Class.forName(DRIVERCLASS);
+			Class.forName(driverClass);
 			
 			// 4) pass credentials into DriverManager's getConnection method
-			conn = DriverManager.getConnection(URLNAME, USERNAME, PASSWORD);
+			conn = DriverManager.getConnection(urlName, userName, password);
 
 			// 5) validate connection object
 			if(conn != null) 
